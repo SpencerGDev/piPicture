5bcde97e40323af34b03bd1dac70c2f41dab9b98
SpencerGDev <spencergdevine@gmail.com>
2025-01-03 18:33:53 +0000

    Upgraded to video terminatable by control-c signal handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <csignal>
 
 using namespace cv;
 using namespace std;
+volatile sig_atomic_t stopFlag = 0;
+
+void signal_handler(int signal)
+{
+   cout << "Stop flag recieved\n";
+   stopFlag = 1;
+}
 
 int main(int argc, char **argv)
 {
+   signal(SIGINT, signal_handler); // control c
    Mat frame;
-   VideoCapture cap; //
+   VideoCapture cap;
 
    cap.open(0); // opens the camera for capture
    if (!cap.isOpened())
@@ -15,17 +24,37 @@ int main(int argc, char **argv)
       cout << "Error opening camera\n";
       return -1;
    }
-   else
-   {
-      cout << "Successful\n";
-   }
+   cout << "Camera successfully opened\n";
 
-   cap.read(frame);
-   if (frame.empty())
+   // setup video writer
+
+   // get the widthxheight of frames of the video
+   int frame_Width = cap.get(CAP_PROP_FRAME_WIDTH);
+   int frame_Height = cap.get(CAP_PROP_FRAME_HEIGHT);
+
+   cout << "Resolution = " << frame_Width << "x" << frame_Height << endl;
+
+   VideoWriter outp;
+   outp.open("output.avi", VideoWriter::fourcc('X', 'V', 'I', 'D'), 20, Size(frame_Width, frame_Height));
+
+   if (!outp.isOpened())
    {
-      cout << "No frame captured\n";
+      cout << "Error opening videoWriter\n";
       return -1;
    }
-   imwrite("image.png", frame);
+
+   while (!stopFlag)
+   {
+      cap.read(frame);
+      if (frame.empty())
+      {
+         cout << "No frame captured\n";
+         return -1;
+      }
+      outp.write(frame); // writes to output file
+   }
+
+   outp.release();
+   cap.release();
    return 0;
 }
